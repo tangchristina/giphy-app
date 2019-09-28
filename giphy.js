5db05fdcdb400f3dfe531407e864e6b4eeffc022
Christina Tang <christinatangerine@hotmail.com>
2019-09-27 19:24:06 -0700

    created a navbar and search box via bootstrap

--- giphy.js
+++ giphy.js
@@ -1,11 +1,46 @@
+   // Adding click event listen listener to all buttons
+$("button").on("click", function() {
+    // Grabbing and storing the data-animal property value from the button
+    var car = $(this).attr("data-car");
+    
+
+console.log(car);
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-cars + "&api_key=o8yLqpswHBgLXjXQNuvzUmOG2XpKEOEk&limit=10";
+car + "&api_key=o8yLqpswHBgLXjXQNuvzUmOG2XpKEOEk&limit=10";
 
 
 $.ajax({
     url: queryURL,
     method: "GET"
     }).then(function(apiResponse){
+        console.log(queryURL);
         console.log(apiResponse);
-        $("#movie-view").text(JSON.stringify(apiResponse));
-});  
+        var results = response.data;
+
+        // Looping through each result item
+        for (var i = 0; i < results.length; i++) {
+
+          // Creating and storing a div tag
+          var carDiv = $("<div>");
+
+          // Creating a paragraph tag with the result item's rating
+          var p = $("<p>").text("Rating: " + results[i].rating);
+
+          // Creating and storing an image tag
+          var carImage = $("<img>");
+          // Setting the src attribute of the image to a property pulled off the result item
+          carImage.attr("src", results[i].images.fixed_height.url);
+
+          // Setting the src attribute of the image to a property pulled off the result item
+          carImage.attr("src", results[i].images.fixed_height.url);
+
+          // Appending the paragraph and image tag to the animalDiv
+          carDiv.append(p);
+          carDiv.append(animalImage);
+
+          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+          $("#gifs-appear-here").prepend(carDiv);
+        }
+      });
+        
+});

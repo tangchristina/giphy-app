983044dae2c72a3121c89c309b1d84076b9840b4
Christina Tang <christinatangerine@hotmail.com>
2019-09-29 19:38:32 -0700

    ajax call is not working, can dynamically add buttons from the text in the textbox

--- giphy.js
+++ giphy.js
@@ -1,21 +1,72 @@
-   // Adding click event listen listener to all buttons
+// Initial array of movies
+var cars = ["Audi", "BMW", "Mercedes", "Acura", "Infiniti", "Porsche", "Dodge", "Ford", "Volvo", "Maserati", "Fiat", "Toyota", "Lexus"];
+//Function for
+function captureCarName () {
+    
+        var carName = $(this).attr("data-name");
+
+       
+      
+}
+// Function for displaying movie data
+function renderButtons() {
+
+    // Deleting the movies prior to adding new movies
+    // (this is necessary otherwise we will have repeat buttons)
+    $("#buttons-view").empty();
+
+    // Looping through the array of cars
+    for (var i = 0; i < cars.length; i++) {
+
+      // Then dynamicaly generating buttons for each car in the array
+      // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+      var a = $("<button>");
+      // Adding a class of car to our button
+      a.addClass("car");
+      // Adding a data-attribute
+      a.attr("data-name", cars[i]);
+      // Providing the initial button text
+      a.text(cars[i]);
+      // Adding the button to the HTML
+      $("#buttons-view").append(a);
+    }
+  }   
+
+// This function handles events where one button is clicked
+$("#search-car").on("click", function(event) {
+    // Preventing the buttons default behavior when clicked (which is submitting a form)
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var car = $("#car-input").val().trim();
+
+    // Adding the movie from the textbox to our array
+    cars.push(car);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    car + "&api_key=o8yLqpswHBgLXjXQNuvzUmOG2XpKEOEk";
+    
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        $("#gifs-appear-here").text(JSON.stringify(response));
+      });
+});
+
+ /*  // Adding click event listener to the button clicked
 $("button").on("click", function() {
-    // Grabbing and storing the data-animal property value from the button
-    var car = $(this).attr("data-car");
+
+    // prevent form from submitting
+    event.preventDefault();
+
+
     
 
 console.log(car);
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-car + "&api_key=o8yLqpswHBgLXjXQNuvzUmOG2XpKEOEk&limit=10";
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(apiResponse){
-        console.log(queryURL);
-        console.log(apiResponse);
-        var results = response.data;
 
         // Looping through each result item
         for (var i = 0; i < results.length; i++) {
@@ -36,11 +87,16 @@ $.ajax({
 
           // Appending the paragraph and image tag to the animalDiv
           carDiv.append(p);
-          carDiv.append(animalImage);
+          carDiv.append(carImage);
 
           // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
           $("#gifs-appear-here").prepend(carDiv);
         }
       });
         
-});
+});*/
+
+$(document).on("click", ".car", captureCarName);
+
+      // Calling the renderButtons function to display the intial buttons
+      renderButtons();
